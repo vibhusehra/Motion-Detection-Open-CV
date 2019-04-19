254d3b56ce47019e31d25af8bd1690d7b3122082
vibhusehra <vibhusehra@gmail.com>
2019-04-19 14:13:24 +0530

    tweaked some values and added comments

--- motionDetection.py
+++ motionDetection.py
@@ -16,24 +16,29 @@ gray = cv2.cvtColor(currentFrame,cv2.COLOR_BGR2GRAY)
 
 i=0
 while True:
-	lastFrame = currentFrame
-	ret,currentFrame = cam.read()
+	lastFrame = currentFrame 
+	ret,currentFrame = cam.read() #reading current frame
 	gray = cv2.cvtColor(currentFrame,cv2.COLOR_BGR2GRAY)
 
 	#finding absolute diff. by the last frame and current frame
 	diff = cv2.absdiff(lastFrame,currentFrame)
+	diffGray = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY) #converted the diff to grayscale
 
+	#checking the difference 
 	# i += 1
 	# if i%10 == 0:
 	# 	i = 0
 	# 	print("current: ",np.mean(currentFrame))
 	# 	print("diff: ", np.mean(diff))
-	if np.mean(diff) > 10:
+
+	#if the observed difference is quite significant, we will print motion detected
+	if np.mean(diff) > 3:
 		print("Motion Detected!!")
 
-	cv2.imshow('Video',diff)
+	cv2.imshow('Video',diffGray) #displaying the difference of last 2 frames(Grayscale)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
-		
+	
+#releasing the webcam and closing the window	
 cam.release()
 cv2.destroyAllWindows()
